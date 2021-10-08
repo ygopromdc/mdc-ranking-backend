37a56a446538f0a0d4c94b137b48c35251f1ca38
diegofcornejo <diegof.cornejo@gmail.com>
2021-10-08 08:04:12 -0600

    get guild members

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ client.login('ODk0ODE2MDI0MTIwMTM5Nzg2.YVvgXQ.BD1uNlbiQfBy4hfRxfVHdDS3syU');
 // Declare a route
 fastify.get('/', async (req, res) => {
     // client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-        const guild = await client.guilds.cache.get('723594962851332197');
-        guild.members.fetch().then(members => {
-            var guildmembers = [];
-            members.forEach(member => {
-                guildmembers.push(member)
-            });
-            // client.destroy();
-            return guildmembers;
-        });
-    // });
-    
+    console.log(`Logged in as ${client.user.tag}!`);
+    const guild = client.guilds.cache.get('723594962851332197');
+    const members = await guild.members.fetch();
+    var data = [];
+    members.map(async (member, i) => {
+        // console.log(member.user.username)
+        let obj = {
+            "id": member.user.id,
+            "username": member.user.username,
+            "discriminator": member.user.discriminator,
+            "avatar":`https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.png`
+            // ...payload
+        };
+        data.push(obj);
+    });
+    return data;
 })
 
 // Run the server!
