514caa9d2c1a7c60d4bf5dc1e390007864ae696a
diegofcornejo <diegof.cornejo@gmail.com>
2021-10-08 07:28:12 -0600

    show discord members

--- server.js
+++ server.js
@@ -4,29 +4,23 @@ const fastify = require('fastify')({ logger: true })
 const { Client, Intents } = require('discord.js');
 // const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
 const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] });
+client.login('ODk0ODE2MDI0MTIwMTM5Nzg2.YVvgXQ.BD1uNlbiQfBy4hfRxfVHdDS3syU');
 
 // Declare a route
 fastify.get('/', async (req, res) => {
-    client.on('ready', () => {
+    // client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
-        const guild = client.guilds.cache.get('723594962851332197');
-
-        // Fetch and get the list named 'members'
+        const guild = await client.guilds.cache.get('723594962851332197');
         guild.members.fetch().then(members => {
-            // Loop through every members
             var guildmembers = [];
             members.forEach(member => {
-                // Do whatever you want with the current member
-                // console.log(member.user.username)
-                // console.log(member.user)
                 guildmembers.push(member)
             });
             // client.destroy();
             return guildmembers;
         });
-    });
-    // fastify.log.info(req.query)
-    client.login('ODk0ODE2MDI0MTIwMTM5Nzg2.YVvgXQ.BD1uNlbiQfBy4hfRxfVHdDS3syU');
+    // });
+    
 })
 
 // Run the server!
