017c0000f5f5e00786ddb63049466359d52d8c58
diegofcornejo <diegof.cornejo@gmail.com>
2022-06-09 00:29:07 -0600

    Initial commit

--- server.js
+++ server.js
@@ -4,33 +4,33 @@ const fastify = require('fastify')({ logger: true })
 const { Client, Intents } = require('discord.js');
 // const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
 const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] });
-client.login('ODk0ODE2MDI0MTIwMTM5Nzg2.YVvgXQ.BD1uNlbiQfBy4hfRxfVHdDS3syU');
+client.login(process.env.DISCORD_TOKEN);
 
 // Declare a route
-fastify.get('/', async (req, res) => {
+fastify.get('/', async (_, res) => {
     // client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     const guild = client.guilds.cache.get('723594962851332197');
     const members = await guild.members.fetch();
     var data = [];
-    members.map(async (member, i) => {
+    members.map(async (member) => {
         // console.log(member.user.username)
         let obj = {
             "id": member.user.id,
             "username": member.user.username,
             "discriminator": member.user.discriminator,
-            "avatar":`https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.png`
+            "avatar": `https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.png`
             // ...payload
         };
         data.push(obj);
     });
-    return data;
+    res.send(data);
 })
 
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(process.env.PORT, '0.0.0.0')
+        await fastify.listen({ port: process.env.PORT })
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
